3437d91e92e8d3bed0e1b3972df01e6b0dd56399
ramreddykiran <ramreddykiran@gmail.com>
2023-01-20 18:45:06 +0530

    added logback feature in producers module

--- producers/src/main/java/org/kafka/learn/MessageProducer.java
+++ producers/src/main/java/org/kafka/learn/MessageProducer.java
@@ -5,12 +5,16 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class MessageProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageProducer.class);
 
     String topicName = "test-topic-api-1";
 
@@ -32,8 +36,11 @@ public class MessageProducer {
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, key, value);
         try {
             RecordMetadata recordMetadata = kafkaProducer.send(producerRecord).get();
-            System.out.println("partition " + recordMetadata.partition() + " , offset : " + recordMetadata.offset());
+           // System.out.println("partition " + recordMetadata.partition() + " , offset : " + recordMetadata.offset());
+            logger.info("message {} sent successfully  for the key : {} ", value, key);
+            logger.info("published message for the partition : {} , offset : {}", recordMetadata.partition(), recordMetadata.offset());
         } catch (InterruptedException e) {
+            logger.error("exception occurred under publishMessageSync {}", e.getMessage());
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
@@ -43,6 +50,5 @@ public class MessageProducer {
     public static void main(String[] args) {
         MessageProducer messageProducer = new MessageProducer(createProducerPropertiesMap());
         messageProducer.publishMessageSync(null, "sending message2 from api call");
-
     }
 }
