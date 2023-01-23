99d6ce8d660e8c34d0b50c373966624c40ac5aaf
ramreddykiran <ramreddykiran@gmail.com>
2023-01-23 17:51:43 +0530

    committing the offsets synchronously

---
+++ consumers/src/main/java/org/kafka/consumer/commitoffsets/MessageConsumerReplicationSyncCommitOffset.java
@@ -0,0 +1,65 @@
+package org.kafka.consumer.commitoffsets;
+
+import org.apache.kafka.clients.consumer.CommitFailedException;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.utils.CollectionUtils;
+import org.kafka.consumer.util.ConsumerConfigUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+
+public class MessageConsumerReplicationSyncCommitOffset {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumerReplicationSyncCommitOffset.class);
+
+    private static final String topicName = "test-topic-api-1-replication";
+
+    private KafkaConsumer<String, String> createConsumer() {
+        Map<String, Object> consumerPropertiesMap = ConsumerConfigUtil.createConsumerPropertiesMap();
+        consumerPropertiesMap.put(ConsumerConfig.GROUP_ID_CONFIG, "messageconsumer");
+        //consumerPropertiesMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        //consumerPropertiesMap.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 5000);
+        //consumerPropertiesMap.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 10000);
+        consumerPropertiesMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        return ConsumerConfigUtil.createKafkaConsumer(consumerPropertiesMap);
+    }
+
+    private void pollKafka(KafkaConsumer<String, String> kafkaConsumer) {
+        kafkaConsumer.subscribe(List.of(topicName));
+        Duration timeOutDuration = Duration.of(100, ChronoUnit.MILLIS);
+        try {
+            while(true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(timeOutDuration);
+                consumerRecords.forEach(record -> {
+                    logger.info("consumer record: key {} and value {} and partition {}",
+                            record.key(), record.value(), record.partition());
+                });
+                if(consumerRecords.count() > 0) {
+                    kafkaConsumer.commitSync(); //commits the last record offset returned by the poll
+                    logger.info("offset committed");
+                }
+            }
+        } catch(CommitFailedException e){
+            logger.error("commitfailedexception in pollKafka {}",e);
+        }   catch(Exception e) {
+            logger.error("exception in pollKafka {}", e);
+
+        } finally {
+            kafkaConsumer.close();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MessageConsumerReplicationSyncCommitOffset messageConsumerReplication = new MessageConsumerReplicationSyncCommitOffset();
+        KafkaConsumer<String, String> kafkaConsumer = messageConsumerReplication.createConsumer();
+        messageConsumerReplication.pollKafka(kafkaConsumer);
+    }
+
+}
